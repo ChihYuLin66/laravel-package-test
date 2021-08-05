4b0f8564b77b7df37786bae51d67ec2cbee43d75
Dan <yansmile0124@gmail.com>
2021-08-05 23:34:42 +0800

    First

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ChihYuLine66\LaravelPackageTest\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
